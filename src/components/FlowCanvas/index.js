5b663be9d1f90290a437fd81a74faf9b415fd083
arifulbgt4 <arifulbgt4@gmail.com>
2020-05-16 22:16:52 +0600

    feat: canvas mouse event

--- src/components/FlowCanvas/index.js
+++ src/components/FlowCanvas/index.js
@@ -3,7 +3,27 @@ import { Flow } from 'gg-editor';
 import './style.css';
 
 const FlowCanvas = () => {
-  return <Flow style={{ background: '#ddd' }} className='flow' />;
+  return (
+    <Flow
+      // onAfterItemSelected={async (e) => {
+      //   const item = await e.item;
+      //   item.isSelected = false;
+      //   console.log('af', item.id);
+      //   console.log('af', item.isSelected);
+      // }}
+      onMouseEnter={(e) => {
+        console.log(e._type);
+      }}
+      onBeforeItemUnselected={async (e) => {
+        const item = await e.item;
+        item.isSelected = true;
+        console.log('before', item.id);
+        console.log('before', item.isSelected);
+      }}
+      style={{ background: '#ddd' }}
+      className='flow'
+    />
+  );
 };
 
 export default FlowCanvas;
